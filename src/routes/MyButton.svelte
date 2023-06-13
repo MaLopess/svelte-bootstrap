53a0d659f45b9d2babe571690d691a6ea2d5f3e6
Marli Lopes <maa2603@hotmail.com>
2023-06-13 16:20:57 -0300

    Em progresso

---
+++ src/routes/MyButton.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let color;
+	
+	function getClass(prefix, suffix) {
+		if(!suffix) {
+			return '';
+		}
+		return `${prefix}-${suffix}`;
+	}
+</script>
+
+
+<button type="button" class="btn {getClass('btn', color)}">
+	<slot /> class="btn {getClass('btn', color)}"
+</button>
