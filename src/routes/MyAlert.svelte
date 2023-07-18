42a29adae5bfc67876b37501576552fce0c2d0f5
Marli Lopes <marli.lopes@walker-di.com.br>
2023-07-18 11:31:46 -0300

    Feat

--- src/routes/MyAlert.svelte
+++ src/routes/MyAlert.svelte
@@ -28,24 +28,26 @@
   <div><slot /></div>
 </div>
 
-<div class="alert alert-success d-flex align-items-center" role="alert">
+<div class="alert alert-{color} success d-flex align-items-center" role="alert">
   <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"
     ><use xlink:href="#check-circle-fill" /></svg
   >
-  <div>An example success alert with an icon</div>
+  <div><slot /></div>
 </div>
-<div class="alert alert-warning d-flex align-items-center" role="alert">
+
+<div class="alert alert-{color}  d-flex align-items-center" role="alert">
   <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Warning:"
     ><use xlink:href="#exclamation-triangle-fill" /></svg
-  >
-  <div>An example warning alert with an icon</div>
-</div>
-<div class="alert alert-danger d-flex align-items-center" role="alert">
+    >
+    <div><slot /></div>
+  </div>
+
+<div class="alert alert-{color}  d-flex align-items-center" role="alert">
   <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:"
     ><use xlink:href="#exclamation-triangle-fill" /></svg
-  >
-  <div>An example danger alert with an icon</div>
-</div>
+    >
+    <div><slot /></div>
+  </div>
 
 <style>
 	.bi {
